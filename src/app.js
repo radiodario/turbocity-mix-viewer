6f40796a010a9d31b11dee9eb6db21d33ed7f0f1
radiodario <dario.villanueva@gmail.com>
2015-04-20 12:01:33 +0100

    rotate, use newer mesh

--- src/app.js
+++ src/app.js
@@ -143,7 +143,8 @@ function render() {
   uniforms.time.value += delta * 5;
 
   var time = clock.getElapsedTime();
-  mesh.rotation.z -= .005;
+  // mesh.rotation.z -= .005;
+  mesh.rotation.z = ((Math.PI/4) - Math.abs((time % Math.PI) - (Math.PI)));
 
   renderer.render(scene, camera);
   requestAnimationFrame(render);
