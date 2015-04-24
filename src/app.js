7a392f2b89e87081e7ab04c94c1e6830797f7198
radiodario <dario.villanueva@gmail.com>
2015-04-24 10:44:04 +0100

    lemons

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ renderer = new THREE.WebGLRenderer({
   alpha: true
 });
 
-renderer.setClearColor( 0xffffff, 1 );
+renderer.setClearColor(0x000000, 0);
 
 renderer.setSize(WIDTH, HEIGHT);
 
@@ -99,7 +99,7 @@ loader.load('data/turbocity.json', function (geometry, materials) {
   var material = new THREE.ShaderMaterial( {
     uniforms: uniforms,
     vertexShader: document.getElementById( 'vertex-shader' ).textContent,
-    fragmentShader: document.getElementById( 'fragment-shader-3' ).textContent
+    fragmentShader: document.getElementById( 'fragment-shader-1' ).textContent
     } );
 
 
